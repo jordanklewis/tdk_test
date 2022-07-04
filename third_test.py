d236193ee1c736cf8799b76b9d8b726e6bd91777
jordanklewis <108379647+jordanklewis@users.noreply.github.com>
2022-07-04 10:07:58 -0500

    Function4 and Function1 edit

--- third_test.py
+++ third_test.py
@@ -1,7 +1,11 @@
 def function1(self, arg):
-    data = arg[1]
-    header = arg[2]
+    data = arg[0]
+    header = arg[1]
     return data + header
 
 def function3(self, arg):
+    arg = arg * 4
+    return arg
+
+def funciton4(self, arg);
     return arg
